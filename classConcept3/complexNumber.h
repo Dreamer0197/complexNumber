392f1053079a8f88a240402dc7656c124ae5de4c
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-13 20:44:52 +0300

    Completed

--- classConcept3/complexNumber.h
+++ classConcept3/complexNumber.h
@@ -4,9 +4,76 @@
 //
 //  Created by Kaan Şengün on 13.11.2023.
 //
+#include <iostream>
+using namespace std;
 
-#ifndef complexNumber_h
-#define complexNumber_h
-
-
-#endif /* complexNumber_h */
+class complexNumber{
+private:
+    int real,imag;
+public:
+    complexNumber(int r=0, int i=0);
+    ~complexNumber();
+    complexNumber(const complexNumber &oth);
+    int getReal() const{
+        return real;
+    }
+    int getImag() const{
+        return imag;
+    }
+    void setValue(int r=0,int i=0);
+    complexNumber addComplex(const complexNumber &oth);
+    complexNumber multiplyComplex(const complexNumber &oth);
+    complexNumber substractComplex(complexNumber &oth);
+    complexNumber multiplyConstant(int constant);
+    void multiplyMinus();
+    void displayComplex();
+    
+};
+complexNumber::complexNumber(int r,int i){
+    real=r;
+    imag=i;
+    cout<<"Complex Number created."<<endl;
+}
+complexNumber::~complexNumber(){
+    cout<<"Complex Number destroyed."<<endl;
+}
+complexNumber::complexNumber(const complexNumber &oth){
+    real=oth.real;
+    imag=oth.imag;
+    cout<<"Complex Number copied to target."<<endl;
+}
+void complexNumber::setValue(int r,int i){
+    real=r;
+    imag=i;
+}
+void complexNumber::displayComplex(){
+    imag>=0? cout<<real<<"+"<<imag<<"i"<<endl: cout<<real<<imag<<"i"<<endl;
+}
+complexNumber complexNumber::addComplex(const complexNumber &oth){
+    complexNumber result;
+    result.real=real+oth.real;
+    result.imag=imag+oth.imag;
+    return result;
+}
+complexNumber complexNumber::substractComplex(complexNumber &oth){
+    complexNumber result;
+    oth.multiplyMinus();
+    result=addComplex(oth);
+    return result;
+}
+void complexNumber::multiplyMinus(){
+    real *=-1;
+    imag *=-1;
+}
+complexNumber complexNumber::multiplyConstant(int constant){
+    complexNumber result;
+    result.real=real*constant;
+    result.imag=imag*constant;
+    return result;
+}
+complexNumber complexNumber::multiplyComplex(const complexNumber &oth){
+    complexNumber result;
+    result.real=real*oth.real-imag*oth.imag;
+    result.imag=real*oth.imag+imag*oth.real;
+    return result;
+}
--- classConcept3/main.cpp
+++ classConcept3/main.cpp
@@ -6,9 +6,46 @@
 //
 
 #include <iostream>
+#include "complexNumber.h"
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    complexNumber number1(4,5);
+    cout<<"Real:"<<number1.getReal()<<endl;
+    cout<<"Imag:"<<number1.getImag()<<endl;
+    number1.displayComplex();
+    
+    complexNumber number2(3,-6);
+    cout<<"Real:"<<number2.getReal()<<endl;
+    cout<<"Imag:"<<number2.getImag()<<endl;
+    number2.displayComplex();
+    
+    complexNumber number3(number2);
+    number3.displayComplex();
+    number3.setValue(4,-7);
+    number3.displayComplex();
+    
+    cout<<"--------------------------------"<<endl;
+    complexNumber resultMain;
+    resultMain=number1.addComplex(number2);
+    resultMain.displayComplex();
+    
+    cout<<"--------------------------------"<<endl;
+    complexNumber resultMain2;
+    resultMain2=number1.substractComplex(number2);
+    resultMain2.displayComplex();
+    
+    cout<<"--------------------------------"<<endl;
+    complexNumber resultMain3;
+    resultMain3=number1.multiplyConstant(4);
+    resultMain3.displayComplex();
+    
+    cout<<"--------------------------------"<<endl;
+    complexNumber resultMain4;
+    resultMain4=number1.multiplyComplex(number2);
+    resultMain4.displayComplex();
+    
     return 0;
 }
